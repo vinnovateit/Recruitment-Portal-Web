c6017c7113ef5611a01cb418a1de14149966060e
konnster99 <42315460+konnster99@users.noreply.github.com>
2019-10-22 03:20:51 +0530

    trial 2
    added all options and connected need the questions and options now havae some editing left do do will do it tomorrow with the help of arpit.

--- first_trial.js
+++ first_trial.js
@@ -1,46 +1,74 @@
 var ques= ["HIIII","d any data","This page isn’t wor","rgbrre","regegeg","rwgrger","effewffff","dd","gdbd","gsrgs"]
-$(document).ready(function(){
-$("#c1").hide();
- { document.getElementById("qd").innerHTML=ques[0];
-   
-    $("#1").click(function(){
-       $("#1").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[0]; 
-    })
-    $("#2").click(function(){
-        $("#2").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[1]; 
-    })
-    $("#3").click(function(){
-        $("#3").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[2]; 
-    })
-    $("#4").click(function(){
-        $("#4").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[3]; 
-    })
-    $("#5").click(function(){
-        $("#5").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[4]; 
-    })
-    $("#6").click(function(){
-        $("#6").css({backgroundColor:'#5F4D93'})
-       document.getElementById("qd").innerHTML=ques[5]; 
-    })
-    $("#7").click(function(){
-       document.getElementById("qd").innerHTML=ques[6]; 
-    })
-    $("#8").click(function(){
-       document.getElementById("qd").innerHTML=ques[7]; 
-    })
-    $("#9").click(function(){
-       document.getElementById("qd").innerHTML=ques[8]; 
-    })
-    $("#10").click(function(){
-       document.getElementById("qd").innerHTML=ques[9]; 
-    })
+var quesops=["wfdcs","qwafvs","efeaf","eqfqewfw"]
+var quesops1=["wfdcsr","qwafvs","efeaf","eqfqewfw"]
+var quesops2=["wfdcsw","qwafvs","efeaf","eqfqewfw"]
+var quesops3=["wfdces","qwafvs","efeaf","eqfqewfw"]
+var quesops4=["wfdcs","qwafvs","efeaf","eqfqewfw"]
+var quesops5=["wfdcsr","qwafvs","efeaf","eqfqewfw"]
+var quesops6=["wfdcsw","qwafvs","efeaf","eqfqewfw"]
+var quesops7=["wfdces","qwafvs","efeaf","eqfqewfw"]
+var quesops8=["wfdcs","safsf","efeaf","eqfqewfw"]
+var quesops9=["wfdcsr","qwafvs","efeaf","eqfqewfw"]
 
-}//container elements
+var wed=["w","ww","www","qwed","daf","wrfasd","wfcdfv","warfvgfc","wrfggfdef","wrtgfdsxc"]
+var wedops=["wed","qwafvs","efeaf","eqfqewfw"]
+var wedops1=["wed1","qwafvs","efeaf","eqfqewfw"]
+var wedops2=["wed2","qwafvs","efeaf","eqfqewfw"]
+var wedops3=["wed3","qwafvs","efeaf","eqfqewfw"]
+var wedops4=["wed4","qwafvs","efeaf","eqfqewfw"]
+var wedops5=["wed5","qwafvs","efeaf","eqfqewfw"]
+var wedops6=["wed6","qwafvs","efea77f","eqfqewfw"]
+var wedops7=["wed7","qwafvs","efeaf","eqfqewfw"]
+var wedops8=["wed8","qwafvs","efeaf","eqfqewfw"]
+var wedops9=["wed9","qwafvs","efeaf","eqfqewfw"]
+var ded=["we","ww","www","qwed","daf","wrfasd","wfcdfv","warfvgfc","wrfggfdef","wrtgfdsxc"]
+var dedops=["ded","qwafvs","efeaf","eqfqewfw"]
+var dedops1=["ded1","qwafvs","efeaf","eqfqewfw"]
+var dedops2=["ded2","qwafvs","efeaf","eqfqewfw"]
+var dedops3=["ded3","qwafvs","efeaf","eqfqewfw"]
+var dedops4=["ded4","qwafvs","efeaf","eqfqewfw"]
+var dedops5=["ded5","qwafvs","efeaf","eqfqewfw"]
+var dedops6=["ded6","qwafvs","efea77f","eqfqewfw"]
+var dedops7=["ded7","qwafvs","efeaf","eqfqewfw"]
+var dedops8=["ded8","qwafvs","efeaf","eqfqewfw"]
+var dedops9=["ded9","qwafvs","efeaf","eqfqewfw"]
+
+var aed=["w","ww","www","qwed","daf","wrfasd","wfcdfv","warfvgfc","wrfggfdef","wrtgfdsxc"]
+var aedops=["aed","qwafvs","efeaf","eqfqewfw"]
+var aedops1=["aed1","qwafvs","efeaf","eqfqewfw"]
+var aedops2=["aed2","qwafvs","efeaf","eqfqewfw"]
+var aedops3=["aed3","qwafvs","efeaf","eqfqewfw"]
+var aedops4=["aed4","qwafvs","efeaf","eqfqewfw"]
+var aedops5=["aed5","qwafvs","efeaf","eqfqewfw"]
+var aedops6=["aed6","qwafvs","efea77f","eqfqewfw"]
+var aedops7=["aed7","qwafvs","efeaf","eqfqewfw"]
+var aedops8=["aed8","qwafvs","efeaf","eqfqewfw"]
+var aedops9=["aed9","qwafvs","efeaf","eqfqewfw"]
+
+var iot=["w","ww","www","qwed","daf","wrfasd","wfcdfv","warfvgfc","wrfggfdef","wrtgfdsxc"]
+var iotops=["iot","qwafvs","efeaf","eqfqewfw"]
+var iotops1=["iot1","qwafvs","efeaf","eqfqewfw"]
+var iotops2=["iot2","qwafvs","efeaf","eqfqewfw"]
+var iotops3=["iot3","qwafvs","efeaf","eqfqewfw"]
+var iotops4=["iot4","qwafvs","efeaf","eqfqewfw"]
+var iotops5=["iot5","qwafvs","efeaf","eqfqewfw"]
+var iotops6=["iot6","qwafvs","efea77f","eqfqewfw"]
+var iotops7=["iot7","qwafvs","efeaf","eqfqewfw"]
+var iotops8=["iot8","qwafvs","efeaf","eqfqewfw"]
+var iotops9=["iot9","qwafvs","efeaf","eqfqewfw"]
+var fin=["w","ww","www","qwed","daf","wrfasd","wfcdfv","warfvgfc","wrfggfdef","wrtgfdsxc"]
+var finops=["fin","qwafvs","efeaf","eqfqewfw"]
+var finops1=["fin1","qwafvs","efeaf","eqfqewfw"]
+var finops2=["fin2","qwafvs","efeaf","eqfqewfw"]
+var finops3=["fin3","qwafvs","efeaf","eqfqewfw"]
+var finops4=["fin4","qwafvs","efeaf","eqfqewfw"]
+var finops5=["fin5","qwafvs","efeaf","eqfqewfw"]
+var finops6=["fin6","qwafvs","efea77f","eqfqewfw"]
+var finops7=["fin7","qwafvs","efeaf","eqfqewfw"]
+var finops8=["fin8","qwafvs","efeaf","eqfqewfw"]
+var finops9=["fin9","qwafvs","efeaf","eqfqewfw"]
+
+$(document).ready(function(){
 $("#ai").click(function(){
     $("#wd").fadeOut(200);
     $("#ad").fadeOut(200);
@@ -48,10 +76,212 @@ $("#ai").click(function(){
     $("#des").fadeOut(200);
     $("#iot").fadeOut(200);
     $("#ai").fadeOut(500);
+    
     $("#c1").fadeIn(4000);
 
 
 })
+$("#fin").click(function(){
+    $("#wd").fadeOut(200);
+    $("#ad").fadeOut(200);
+    $("#ai").fadeOut(200);
+    $("#des").fadeOut(200);
+    $("#iot").fadeOut(200);
+    $("#fin").fadeOut(500);
+    $("#c1").fadeIn(4000);
+    ques=fin;
+    quesops=finops;
+    quesops1=finops1;
+    quesops2=finops2;
+    quesops3=finops3;
+    quesops4=finops4;
+    quesops5=finops5;
+    quesops6=finops6;
+    quesops7=finops7;
+    quesops8=finops8;
+    quesops9=finops9;
+
+    
+
+
+})
+$("#iot").click(function(){
+    $("#wd").fadeOut(200);
+    $("#ad").fadeOut(200);
+    $("#fin").fadeOut(200);
+    $("#des").fadeOut(200);
+    $("#ai").fadeOut(200);
+    $("#iot").fadeOut(500);
+    $("#c1").fadeIn(4000);
+    ques=iot;
+    quesops=iotops;
+    quesops1=iotops1;
+    quesops2=iotops2;
+    quesops3=iotops3;
+    quesops4=iotops4;
+    quesops5=iotops5;
+    quesops6=iotops6;
+    quesops7=iotops7;
+    quesops8=iotops8;
+    quesops9=iotops9;
+
+
+
+
+
+})
+$("#wd").click(function(){
+    $("#ai").fadeOut(200);
+    $("#ad").fadeOut(200);
+    $("#fin").fadeOut(200);
+    $("#des").fadeOut(200);
+    $("#iot").fadeOut(200);
+    $("#wd").fadeOut(500);
+    ques=wed;
+    quesops=wedops;
+    quesops1=wedops1;
+    quesops2=wedops2;
+    quesops3=wedops3;
+    quesops4=wedops4;
+    quesops5=wedops5;
+    quesops6=wedops6;
+    quesops7=wedops7;
+    quesops8=wedops8;
+    quesops9=wedops9;
+
+    $("#c1").fadeIn(4000);
+
+
+
+})
+$("#des").click(function(){
+    $("#wd").fadeOut(200);
+    $("#ad").fadeOut(200);
+    $("#fin").fadeOut(200);
+    $("#ai").fadeOut(200);
+    $("#iot").fadeOut(200);
+    $("#des").fadeOut(500);
+    $("#c1").fadeIn(4000);
+    ques=ded;
+    quesops=dedops;
+    quesops1=dedops1;
+    quesops2=dedops2;
+    quesops3=dedops3;
+    quesops4=dedops4;
+    quesops5=dedops5;
+    quesops6=dedops6;
+    quesops7=dedops7;
+    quesops8=dedops8;
+    quesops9=dedops9;
+
+
+
+})
+$("#ad").click(function(){
+    $("#wd").fadeOut(200);
+    $("#ai").fadeOut(200);
+    $("#fin").fadeOut(200);
+    $("#des").fadeOut(200);
+    $("#iot").fadeOut(200);
+    $("#ad").fadeOut(500);
+    $("#c1").fadeIn(4000);
+    ques=aed;
+    quesops=aedops;
+    quesops1=aedops1;
+    quesops2=aedops2;
+    quesops3=aedops3;
+    quesops4=aedops4;
+    quesops5=aedops5;
+    quesops6=aedops6;
+    quesops7=aedops7;
+    quesops8=aedops8;
+    quesops9=aedops9;
+
+
+
+})
+$("#c1").hide();
+ { document.getElementById("qd").innerHTML="select question from the list";
+
+   
+    $("#1").click(function(){
+       $("#1").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[0];
+       document.getElementById("op1").innerHTML=quesops[0];
+       document.getElementById("op2").innerHTML=quesops[1];
+       document.getElementById("op3").innerHTML=quesops[2];
+       document.getElementById("op4").innerHTML=quesops[3]; 
+    })
+    $("#2").click(function(){
+        $("#2").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[1];
+       document.getElementById("op1").innerHTML=quesops1[0];
+       document.getElementById("op2").innerHTML=quesops1[1];
+       document.getElementById("op3").innerHTML=quesops1[2];
+       document.getElementById("op4").innerHTML=quesops1[3]; 
+    })
+    $("#3").click(function(){
+        $("#3").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[2];
+       document.getElementById("op1").innerHTML=quesops2[0];
+       document.getElementById("op2").innerHTML=quesops2[1];
+       document.getElementById("op3").innerHTML=quesops2[2];
+       document.getElementById("op4").innerHTML=quesops2[3]; 
+    })
+    $("#4").click(function(){
+        $("#4").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[3];
+       document.getElementById("op1").innerHTML=quesops3[0];
+       document.getElementById("op2").innerHTML=quesops3[1];
+       document.getElementById("op3").innerHTML=quesops3[2];
+       document.getElementById("op4").innerHTML=quesops3[3]; 
+    })
+    $("#5").click(function(){
+        $("#5").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[4];
+       document.getElementById("op1").innerHTML=quesops4[0];
+       document.getElementById("op2").innerHTML=quesops4[1];
+       document.getElementById("op3").innerHTML=quesops4[2];
+       document.getElementById("op4").innerHTML=quesops4[3]; 
+    })
+    $("#6").click(function(){
+        $("#6").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[5];
+       document.getElementById("op1").innerHTML=quesops5[0];
+       document.getElementById("op2").innerHTML=quesops5[1];
+       document.getElementById("op3").innerHTML=quesops5[2];
+       document.getElementById("op4").innerHTML=quesops5[3]; 
+    })
+    $("#7").click(function(){
+       document.getElementById("qd").innerHTML=ques[6];
+       document.getElementById("op1").innerHTML=quesops6[0];
+       document.getElementById("op2").innerHTML=quesops6[1];
+       document.getElementById("op3").innerHTML=quesops6[2];
+       document.getElementById("op4").innerHTML=quesops6[3]; 
+    })
+    $("#8").click(function(){
+       document.getElementById("qd").innerHTML=ques[7];document.getElementById("op1").innerHTML=quesops7[0];
+       document.getElementById("op2").innerHTML=quesops7[1];
+       document.getElementById("op3").innerHTML=quesops7[2];
+       document.getElementById("op4").innerHTML=quesops7[3]; 
+     
+    })
+    $("#9").click(function(){
+       document.getElementById("qd").innerHTML=ques[8];document.getElementById("op1").innerHTML=quesops8[0];
+       document.getElementById("op2").innerHTML=quesops8[1];
+       document.getElementById("op3").innerHTML=quesops8[2];
+       document.getElementById("op4").innerHTML=quesops8[3]; 
+     
+    })
+    $("#10").click(function(){
+       document.getElementById("qd").innerHTML=ques[9]; document.getElementById("op1").innerHTML=quesops9[0];
+       document.getElementById("op2").innerHTML=quesops9[1];
+       document.getElementById("op3").innerHTML=quesops9[2];
+       document.getElementById("op4").innerHTML=quesops9[3]; 
+    
+    })
+
+}//container elements
 
 
 
