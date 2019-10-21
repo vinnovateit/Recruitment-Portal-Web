94897bd3ef66f59d426933f2d00069cf4a9d8409
konnster99 <42315460+konnster99@users.noreply.github.com>
2019-10-21 19:43:30 +0530

    first one
    will fix everything by 2night
    only ai ml button is working as of rn

---
+++ first_trial.js
@@ -0,0 +1,58 @@
+var ques= ["HIIII","d any data","This page isn’t wor","rgbrre","regegeg","rwgrger","effewffff","dd","gdbd","gsrgs"]
+$(document).ready(function(){
+$("#c1").hide();
+ { document.getElementById("qd").innerHTML=ques[0];
+   
+    $("#1").click(function(){
+       $("#1").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[0]; 
+    })
+    $("#2").click(function(){
+        $("#2").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[1]; 
+    })
+    $("#3").click(function(){
+        $("#3").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[2]; 
+    })
+    $("#4").click(function(){
+        $("#4").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[3]; 
+    })
+    $("#5").click(function(){
+        $("#5").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[4]; 
+    })
+    $("#6").click(function(){
+        $("#6").css({backgroundColor:'#5F4D93'})
+       document.getElementById("qd").innerHTML=ques[5]; 
+    })
+    $("#7").click(function(){
+       document.getElementById("qd").innerHTML=ques[6]; 
+    })
+    $("#8").click(function(){
+       document.getElementById("qd").innerHTML=ques[7]; 
+    })
+    $("#9").click(function(){
+       document.getElementById("qd").innerHTML=ques[8]; 
+    })
+    $("#10").click(function(){
+       document.getElementById("qd").innerHTML=ques[9]; 
+    })
+
+}//container elements
+$("#ai").click(function(){
+    $("#wd").fadeOut(200);
+    $("#ad").fadeOut(200);
+    $("#fin").fadeOut(200);
+    $("#des").fadeOut(200);
+    $("#iot").fadeOut(200);
+    $("#ai").fadeOut(500);
+    $("#c1").fadeIn(4000);
+
+
+})
+
+
+
+})
